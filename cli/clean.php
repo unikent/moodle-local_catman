f3c40401e79369d42bb08eb81c13c5f05c33f21d
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-04-24 11:47:08 +0100

    Update to new config format

--- cli/clean.php
+++ cli/clean.php
@@ -22,7 +22,9 @@ require_once(dirname(__FILE__) . '/lib.php');
 // We need admin
 \core\session\manager::set_user(get_admin());
 
-$CFG->local_catman_enable = 1;
-$CFG->local_catman_limit = 999999;
+$CFG->forced_plugin_settings['local_catman'] = array(
+	'enable' => 1,
+	'limit' => 999999
+);
 
 local_catman_cron();
