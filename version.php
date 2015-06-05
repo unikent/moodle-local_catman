c62426140d3e0e6ed1e8a99b01063fd6ac02cfa4
Skylar Kelty <s.kelty@kent.ac.uk>
2015-06-05 09:53:09 +0100

    Bump version

--- version.php
+++ version.php
@@ -18,13 +18,13 @@
  * Version information
  *
  * @package    local_catman
- * @copyright  2014 University of Kent
+ * @copyright  2015 University of Kent
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_catman'; // Full name of the plugin (used for diagnostics).
-$plugin->version   = 2014080400;     // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015060400;     // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;     // Requires this Moodle version.
 $plugin->cron      = 0;
